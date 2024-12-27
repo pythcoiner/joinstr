cc56da02012d0b5bc616fc40d43e87d05a13ffa0
pythcoiner <pythcoiner@proton.me>
2024-12-27 10:39:01 +0100

    nostr client: add documentation

--- src/nostr/client/mod.rs
+++ src/nostr/client/mod.rs
@@ -21,6 +21,11 @@ pub struct NostrClient {
 }
 
 impl NostrClient {
+    /// Create a new nostr client.
+    ///
+    /// # Arguments
+    /// * `name` - Name of the [`NostrClient`] instance (use for debug logs), can
+    ///   be an empty &str.
     pub fn new(name: &str) -> NostrClient {
         NostrClient {
             name: name.into(),
@@ -28,6 +33,12 @@ impl NostrClient {
         }
     }
 
+    /// Add a nostr relay url to [`NostrClient::relays`]
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the client is already connected
+    ///   to some relays.
     pub fn relay(mut self, url: String) -> Result<Self, Error> {
         if self.client.is_none() {
             self.relays.push(url);
@@ -37,6 +48,12 @@ impl NostrClient {
         }
     }
 
+    /// Copy the given list of relays into [`NostrClient::relays`] .
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if [`NostrClient::relays`]
+    ///   if the client is already connected to some relays.
     pub fn relays(mut self, relays: &Vec<String>) -> Result<Self, Error> {
         if self.client.is_none() {
             for url in relays {
@@ -48,10 +65,12 @@ impl NostrClient {
         }
     }
 
-    pub fn get_relays(&self) -> &Vec<String> {
-        &self.relays
-    }
-
+    /// Set the nostr key pair of this client.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the client is already
+    ///   connected to some relays.
     pub fn keys(mut self, keys: Keys) -> Result<Self, Error> {
         if self.client.is_none() {
             self.keys = Some(keys);
@@ -61,10 +80,29 @@ impl NostrClient {
         }
     }
 
+    /// Returns a reference to [`NostrClient::relays`].
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if [`NostrClient::relays`]
+    ///   if the client is already connected to some relays.
+    pub fn get_relays(&self) -> &Vec<String> {
+        &self.relays
+    }
+
+    /// Take the receiver end of the notification channels from the inner client.
     pub fn nostr_receiver(&mut self) -> Option<broadcast::Receiver<RelayPoolNotification>> {
         self.nostr_receiver.take()
     }
 
+    /// Connect to nostr relays defined in [`NostrClient::relays`].
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    ///   - no nostr keypair have been set.
+    ///   - adding a relay fails
+    ///   - suscribing to NIP04 Dms fails
     pub async fn connect_nostr(&mut self) -> Result<(), Error> {
         let opts = Options::new()
             .skip_disconnected_relays(true)
@@ -72,6 +110,7 @@ impl NostrClient {
             .send_timeout(Some(Duration::from_secs(5)));
 
         let client = Client::with_opts(self.get_keys()?, opts);
+        // TODO: Do not use a deprecated method
         #[allow(deprecated)]
         match client.add_relays(self.relays.as_slice()).await {
             Ok(_) => {
@@ -81,10 +120,13 @@ impl NostrClient {
                 self.subscribe_dm().await?;
                 Ok(())
             }
+            // FIXME: we should not error if a single relay cannot be added
+            // but return a map of relays status (Connected/Failed) instead.
             Err(e) => Err(e.into()),
         }
     }
 
+    /// Utility function, will error if the client is not connected.
     pub fn is_connected(&self) -> Result<(), Error> {
         if self.client.is_some() {
             Ok(())
@@ -93,22 +135,52 @@ impl NostrClient {
         }
     }
 
+    /// Returns a ref to [`NostrClient::client`]
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if not connected.
     pub fn client(&self) -> Result<&Client, Error> {
         self.client.as_ref().ok_or(Error::NotConnected)
     }
 
+    /// Returns a ref to [`NostrClient::keys`]
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the keypair has
+    ///   not been set.
     pub fn get_keys(&self) -> Result<&Keys, Error> {
         self.keys.as_ref().ok_or(Error::KeysMissing)
     }
 
+    /// Post a nostr event.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    ///   - the client is not connected
+    ///   - fail to send event.
     pub async fn post_event(&self, event: EventBuilder) -> Result<(), Error> {
         self.is_connected()?;
         let event = event.to_event(self.get_keys()?)?;
-        // log::warn!("NostrClient.post_event({event:?})");
         self.client()?.send_event(event).await?;
         Ok(())
     }
 
+    /// Send a NIP04 encrypted DM
+    ///
+    /// # Arguments
+    /// * `npub` - nostr pubkey of the receiver
+    /// * `content` - raw (unencrypted) message content as String
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    ///   - the client is not connected
+    ///   - the client do not have signing keys
+    ///   - encryption of the message fails
+    ///   - sending the DM fails
     pub async fn send_dm(&self, npub: &PublicKey, content: String) -> Result<(), Error> {
         let client = self.client()?;
         let signer = client.signer().await?;
@@ -128,12 +200,31 @@ impl NostrClient {
         Ok(())
     }
 
+    /// Send a [`PoolMessage`] wrapped into a NIP04 encrypted DM
+    ///
+    /// # Arguments
+    /// * `npub` - nostr pubkey of the pool
+    /// * `msg` - the PoolMessage to send
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    ///   - teh message cannot be serialized into String json payload
+    ///   - sending the DM fails
     pub async fn send_pool_message(&self, npub: &PublicKey, msg: PoolMessage) -> Result<(), Error> {
         let clear_content = msg.to_string()?;
         log::info!("NostrClient.send_pool_message(): {:#?}", clear_content);
         self.send_dm(npub, clear_content).await
     }
 
+    /// Subscribe to notifications of NIP04 DMs thatare send tu the client pubkey
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if :
+    ///   - the client is not connected
+    ///   - the client does not have signing keys
+    ///   - subscription fail
     pub async fn subscribe_dm(&self) -> Result<(), Error> {
         let client = self.client()?;
         let keys = self.get_keys()?;
@@ -150,6 +241,17 @@ impl NostrClient {
         Ok(())
     }
 
+    /// Subscribe to notifications of NIP04 DMs thatare send tu the client pubkey
+    ///
+    /// # Arguments
+    /// * `back` - the client will not receive notifications for pools that have been initiated
+    ///   `back` seconds in the past.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if :
+    ///   - the client is not connected
+    ///   - subscription fail
     pub async fn subscribe_pools(&self, back: u64) -> Result<(), Error> {
         let client = self.client()?;
         let since = Timestamp::now() - Timestamp::from_secs(back);
@@ -158,6 +260,16 @@ impl NostrClient {
         Ok(())
     }
 
+    /// Try to poll notifications/events received by the client, will return:
+    ///   - Some(event) if there is at list one event is in the channel, in this
+    ///     case the message is remode from the channel.
+    ///   - None if the channel is empty
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    ///   - the client is not connected
+    ///   - the channel is closed
     pub fn receive_event(&mut self) -> Result<Option<Event>, Error> {
         if let Some(receiver) = self.nostr_receiver.as_mut() {
             match receiver.try_recv() {
@@ -184,6 +296,14 @@ impl NostrClient {
         }
     }
 
+    /// Try to decrypt the payload of a NIP04 DM, the event will be returned
+    ///   with its encrypted payload replaced by clear text one.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    ///   - the event is not of Kind 04
+    ///   - decryption fails
     pub fn decrypt_dm(&self, mut event: Event) -> Result<Event, Error> {
         if event.kind != Kind::EncryptedDirectMessage {
             return Err(Error::NotNip04);
@@ -198,6 +318,21 @@ impl NostrClient {
         }
     }
 
+    /// Try to poll notifications/events received by the client and parse it as
+    ///    a PoolMessage, will return:
+    ///    - Some(PoolMessage) if there is a message in the channel
+    ///    - None if the channel is empty
+    ///
+    /// Note: if the message is of type [`PoolMessage::Join`] and the pubkey is not
+    ///   specified, we will replace None by the sender pubkey.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    ///   - the client is not connected
+    ///   - the channel is closed
+    ///   - the the received event is not a NIP04
+    ///   - the event cannot be parsed as a PoolMessage
     pub fn receive_pool_msg(&mut self) -> Result<Option<PoolMessage>, Error> {
         let event = self
             .receive_event()?
@@ -229,6 +364,15 @@ impl NostrClient {
         })
     }
 
+    /// Try to poll notifications/events received by the client and parse it as
+    ///    a Pool, it will return:
+    ///    - Some(Pool) if there is a message in the channel
+    ///    - None if the channel is empty or fail to parse as a Pool message
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    ///   - fails to receive event
     pub fn receive_pool_notification(&mut self) -> Result<Option<Pool>, Error> {
         let event = self
             .receive_event()?
